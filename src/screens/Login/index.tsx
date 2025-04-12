34c66444f9b70c837b189f3c8d07f862e89fc59d
arthur <arthur.rios007@gmail.com>
2025-04-12 10:49:06 -0300

    Aplicativo de finanças - Criando e configurando o app

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Tela de login!</Text>
+    </View>
+  )
+}
