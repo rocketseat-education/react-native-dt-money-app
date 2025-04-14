7515102b9782f40d20a56f8cd86d3f691b1c8dc0
arthur <arthur.rios007@gmail.com>
2025-04-14 18:01:27 -0300

    Aplicativo de finanças - Configurando rotas

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from 'react-native'
+import { PublicStackParamsList } from '@/routes'
+import { useNavigation } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export const Login = () => {
+  const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>()
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Tela de login!</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+        <Text>Regitrar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
