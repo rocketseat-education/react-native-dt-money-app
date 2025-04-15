3729b93f719c46834c0026ff4880a545af530cd5
arthur <arthur.rios007@gmail.com>
2025-04-15 06:05:13 -0300

    Aplicativo de finanças - DismissKeyboard

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,6 +1,7 @@
+import { DismissKeyboardView } from '@/components/DismissKeyboard'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity } from 'react-native'
 
 export type PublicStackParamsList = {
   Login: undefined
@@ -11,11 +12,12 @@ export const Login = () => {
   const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>()
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <DismissKeyboardView>
       <Text>Tela de login!</Text>
+      <TextInput className="bg-gray-500 w-full" />
       <TouchableOpacity onPress={() => navigation.navigate('Register')}>
         <Text>Regitrar</Text>
       </TouchableOpacity>
-    </View>
+    </DismissKeyboardView>
   )
 }
