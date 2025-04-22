e4598ad43eb5637bfd6be6b1aa8f9529ee1e9d6c
arthur <arthur.rios007@gmail.com>
2025-04-22 19:06:13 -0300

    Aplicativo de finanças - Consumindo dados do usuário do estado global

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import { PrivateRoutes } from './PrivateRoutes'
 import { PublicRoutes } from './PublicRoutes'
 import { SystemBars } from 'react-native-edge-to-edge'
+import { useAuthContext } from '@/context/auth.context'
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined)
+  const { token, user } = useAuthContext()
 
   const Routes = useCallback(() => {
-    if (!user) {
+    if (!user || !token) {
       return <PublicRoutes />
     } else {
       return <PrivateRoutes />
     }
-  }, [user])
+  }, [user, token])
 
   return (
     <NavigationContainer>
