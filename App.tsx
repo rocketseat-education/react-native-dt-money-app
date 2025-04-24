717e90da4dff10bf71042583cf71e99cd97b1278
arthur <arthur.rios007@gmail.com>
2025-04-24 16:17:11 -0300

    Aplicativo de finanças - Criando Context de Snackbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import NavigationRoutes from '@/routes'
 import './src/styles/global.css'
 import { AuthContextProvider } from '@/context/auth.context'
+import { SnackbarContextProvider } from '@/context/snackbar.context'
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <NavigationRoutes />
-    </AuthContextProvider>
+    <SnackbarContextProvider>
+      <AuthContextProvider>
+        <NavigationRoutes />
+      </AuthContextProvider>
+    </SnackbarContextProvider>
   )
 }
