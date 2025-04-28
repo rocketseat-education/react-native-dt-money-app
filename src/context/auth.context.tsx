47efda0f18bfe585620bbe4d117d76c163cf0c84
arthur <arthur.rios007@gmail.com>
2025-04-28 09:19:30 -0300

    Aplicativo de finanças - Persistindo usuário

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -9,6 +9,8 @@ import {
 } from 'react'
 import * as authService from '@/shared/services/dt-money/auth.service'
 import { IUser } from '@/shared/interfaces/user-interface'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { IAuthenticateResponse } from '@/shared/interfaces/https/authenticate-response'
 
 type AuthContextType = {
   user: IUser | null
@@ -16,6 +18,7 @@ type AuthContextType = {
   handleAuthenticate: (params: FormLoginParams) => Promise<void>
   handleRegister: (params: FormRegisterParams) => Promise<void>
   handleLogout: () => void
+  restoreUserSession: () => Promise<string | null>
 }
 
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType)
@@ -26,7 +29,7 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
 
   const handleAuthenticate = async (userData: FormLoginParams) => {
     const { token, user } = await authService.authenticate(userData)
-    console.log(token, user)
+    await AsyncStorage.setItem('dt-money-user', JSON.stringify({ user, token }))
 
     setUser(user)
     setToken(token)
@@ -34,11 +37,23 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
 
   const handleRegister = async (formData: FormRegisterParams) => {
     const { token, user } = await authService.registerUser(formData)
+    await AsyncStorage.setItem('dt-money-user', JSON.stringify({ user, token }))
+
     setUser(user)
     setToken(token)
   }
 
   const handleLogout = () => {}
+
+  const restoreUserSession = async () => {
+    const userData = await AsyncStorage.getItem('dt-money-user')
+    if (userData) {
+      const { token, user } = JSON.parse(userData) as IAuthenticateResponse
+      setUser(user)
+      setToken(token)
+    }
+    return userData
+  }
 
   return (
     <AuthContext.Provider
@@ -48,6 +63,7 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
         handleAuthenticate,
         handleRegister,
         handleLogout,
+        restoreUserSession,
       }}
     >
       {children}
