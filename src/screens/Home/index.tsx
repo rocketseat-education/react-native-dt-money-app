63b9c07ee30f9807c9b97317b20ee5c0f86df58d
arthur <arthur.rios007@gmail.com>
2025-04-28 09:04:23 -0300

    Aplicativo de finanças - Criando AppHeader

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,18 @@
+import { AppHeader } from '@/components/AppHeader'
 import { useAuthContext } from '@/context/auth.context'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const Home = () => {
   const { handleLogout } = useAuthContext()
 
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <AppHeader />
       <Text>HomeScreen</Text>
       <TouchableOpacity onPress={handleLogout}>
         <Text>Sair</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   )
 }
