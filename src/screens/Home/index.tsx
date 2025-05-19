5ef9ec624324196944947fb3eeb64ad12ca32872
arthur <arthur.rios007@gmail.com>
2025-05-19 11:14:31 -0300

    Aplicativo de finanças - Iniciando Header da lista de transações

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,9 @@ import { useAuthContext } from '@/context/auth.context'
 import { useTransactionContext } from '@/context/transaction.context'
 import { useErrorHandler } from '@/shared/hooks/useErrorHandler'
 import { useEffect } from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { FlatList, Text, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { ListHeader } from './ListHeader'
 
 export const Home = () => {
   const { handleLogout } = useAuthContext()
@@ -26,12 +27,12 @@ export const Home = () => {
   }, [])
 
   return (
-    <SafeAreaView className="flex-1 bg-background-primary">
-      <AppHeader />
-      <Text>HomeScreen</Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Sair</Text>
-      </TouchableOpacity>
+    <SafeAreaView className="flex-1 bg-background-secondary">
+      <FlatList
+        ListHeaderComponent={ListHeader}
+        data={[]}
+        renderItem={() => <></>}
+      />
     </SafeAreaView>
   )
 }
